84252198c8f3d4f48a1107e32707f3c192204a5e
Ofeitus <2008655z@gmail.com>
2022-02-05 10:57:05 +0300

    Book and User validator tests

--- src/test/java/com/epam/ofeitus/library/service/validator/impl/BookValidatorTest.java
+++ src/test/java/com/epam/ofeitus/library/service/validator/impl/BookValidatorTest.java
@@ -12,4 +12,22 @@ class BookValidatorTest {
         boolean result = bookValidator.validate(new Book("1111111111", "title", 0, 1, "Language", "keyWords"));
         Assertions.assertTrue(result);
     }
+
+    @Test
+    void testValidateCorrectLongIsbn() {
+        boolean result = bookValidator.validate(new Book("2222222222222", "title", 0, 1, "Язык", "keyWords"));
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void testValidateIsbnLength() {
+        boolean result = bookValidator.validate(new Book("33333333333", "title", 0, 1, "Language", "keyWords"));
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    void testValidateIsbnForbiddenSymbols() {
+        boolean result = bookValidator.validate(new Book("A111111111", "title", 0, 1, "Language", "keyWords"));
+        Assertions.assertFalse(result);
+    }
 }
--- src/test/java/com/epam/ofeitus/library/service/validator/impl/UserValidatorTest.java
+++ src/test/java/com/epam/ofeitus/library/service/validator/impl/UserValidatorTest.java
@@ -1,15 +1,42 @@
 package com.epam.ofeitus.library.service.validator.impl;
 
 import com.epam.ofeitus.library.entity.user.User;
+import com.epam.ofeitus.library.entity.user.constituent.UserRole;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Date;
 
 class UserValidatorTest {
     UserValidator userValidator = new UserValidator();
 
     @Test
-    void testValidate() {
-        boolean result = userValidator.validate(new User(0, null, "name", "surname", "phoneNumber", "email", null, null, true));
-        Assertions.assertEquals(true, result);
+    void testValidateCorrect() {
+        boolean result = userValidator.validate(new User(0, new Date(), "Ivan", "Ivan", "375291234567", "samplemail@email.com", "", UserRole.ADMIN, false));
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    void testValidateCountryCode() {
+        boolean result = userValidator.validate(new User(0, new Date(), "Ivan", "Ivan", "735291234567", "sampleMail@email.com", "", UserRole.ADMIN, false));
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    void testValidatePhoneNumberLength() {
+        boolean result = userValidator.validate(new User(0, new Date(), "Ivan", "Ivan", "3752912345678", "sampleMail@email.com", "", UserRole.ADMIN, false));
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    void testValidatePhoneNumberForbiddenSymbols() {
+        boolean result = userValidator.validate(new User(0, new Date(), "Ivan", "Ivan", "37529123456A", "sampleMail@email.com", "", UserRole.ADMIN, false));
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    void testValidateEmailNoDot() {
+        boolean result = userValidator.validate(new User(0, new Date(), "Ivan", "Ivan", "375291234567", "sampleMail@emailcom", "", UserRole.ADMIN, false));
+        Assertions.assertFalse(result);
     }
 }
